21427c389c355aeb77e42b1c184bdbccd98aeae0
Lucas Santos <lucascarvlhsantos@gmail.com>
2024-11-03 14:01:20 -0300

    fisr-commit

---
+++ src/elements/sideBar.tsx
@@ -0,0 +1,46 @@
+type SideBar = {
+    profileImg: string;
+    userName: string;
+    userId: string;
+    logged: boolean;
+  };
+
+export const SideBar: React.FC<SideBar> = ({ profileImg, userName, userId, logged }) => {
+    return (
+        <>
+            <div className='sidebar-top not-logged'>
+                { logged ? (
+                    <>
+                        <div className='sidebar-profile-pic' style={{
+                        backgroundImage: `url(${profileImg})`,
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat'
+                        }}>
+                        </div>
+                        <div className='sidebar-user-text'>
+                            <div className='user-name'>
+                                <p>{userName}</p>
+                            </div>
+                            <div className='user-id'>
+                                <p>{userId}</p>
+                            </div>
+                        </div>
+                    </>
+                ): (
+                    <>
+                        <div className='sidebar-user-text'>
+                            <div className='user-name'>
+                                <p>Conecte-se</p>
+                            </div>
+                            <div className='user-id'>
+                                <button>LogIn</button>
+                                <button>Cadastrar</button>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
+        </>
+    )
+}
